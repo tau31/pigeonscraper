f2ee1a4623d7fdc0a76ecf99e6894383dacc55bd
Jane Doe <jane@example.org>
2020-03-26 10:45:28 +0100

    test for xml documents with race tables

--- tests/testthat/test-parse_races.R
+++ tests/testthat/test-parse_races.R
@@ -11,8 +11,24 @@ css_query_tbl <- extract_orgs(years[1,], remDr = remDr)[1,]
 
 # Extract race options ------------------
 race_html <- extract_race_html_options(css_query_tbl = css_query_tbl,
-                                     remDr = remDr)
+                                       remDr = remDr)
 
 test_that("parsed races html have text on option fields", {
   expect_match(race_html %>% rvest::html_text(), regexp = "[[:alnum:]]")
 })
+
+# Generate xml documents with tables to be parsed ---------------
+
+test_that("race_table_parse outputs xml document with table", {
+  xml_doc <-race_table_parse(race_xml_nodeset = race_html[[1]], remDr= remDr)
+  table_test <-
+    xml_doc[[1]] %>%
+    rvest::html_nodes("table") %>%
+    rvest::html_table(fill = TRUE)  %>%
+    .[[1]]   %>%
+    tibble::as_tibble()
+  expect_match(names(table_test)[1], regexp = "Pos")
+  expect_match(names(table_test)[2], regexp = "Name")
+  expect_match(names(table_test)[3], regexp = "Section")
+})
+

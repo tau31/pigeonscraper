tau31/pigeonscraper

commit 5bc1ff885477c672852142bcf949a58c957e83a7

changed start_chrom_remDr fun and added scraping script.

R/selenium_fun.R
@@ -5,16 +5,38 @@
 #' @param kill Bolean indicating weather a runing docker container with selenium
 #' should be killed.
 #' @import RSelenium
+#' @importFrom utils capture.output
 
-start_chrome_remDr <- function(kill = TRUE) {
-  if(kill == TRUE) {
-    system("docker kill `docker ps -q`")
+start_chrome_remDr <- function(kill) {
+  ps_out <- system("docker ps", intern = TRUE)
+
+  capt_ps_out <- capture.output(cat(ps_out))
+
+  container_running <- stringr::str_detect(
+    capt_ps_out,
+    pattern = "selenium/standalone-chrome")
+
+  if(kill == TRUE  | container_running == TRUE) {
+    print("selenium/standalone-chrome docker container already runnign")
+    print("kill container and restart a new one")
+    selenium_cont <- ps_out[which(
+      stringr::str_detect(
+        ps_out,
+        pattern = "selenium/standalone-chrome") == TRUE)]
+
+    docker_id <- stringr::str_split(
+      selenium_cont,
+      pattern = stringr::regex( "[[:space:]]" ))[[1]][1]
+
+    system(stringr::str_c("docker kill ", docker_id))
+    Sys.sleep(2)
+    system("docker run -d -p 4445:4444 --shm-size 2g selenium/standalone-chrome")
+    Sys.sleep(2)
+  } else {
+    print("startign selenium/standalone-chrome docker container")
+    system("docker run -d -p 4445:4444 --shm-size 2g selenium/standalone-chrome")
     Sys.sleep(2)
   }
-  system("docker run -d -p 4445:4444 --shm-size 2g selenium/standalone-chrome")
-  Sys.sleep(2)
-  system("docker ps")
-  Sys.sleep(2)
 }
 
 # connect to remote driver ---------------------------
@@ -108,3 +130,26 @@ get_page_source <- function(remDr, link) {
   parsed_html <- xml2::read_html(source_html)
   return(parsed_html)
 }
+
+# Connect to Link -----
+
+#' Connect remote driver to specified url
+#'
+#' @param remDr remote driver connection
+#' @param link link to the desired webpage
+#'
+#' @return no return
+#'
+#' @import RSelenium
+remDr_go_to_link <-
+  function(remDr, link) {
+    # open remDr
+    remDr$open(silent = TRUE)
+
+     # set a timeout
+    remDr$setTimeout(type = "Implicit", milliseconds = 5000)
+
+    # navigate to the website
+    remDr$navigate(url = link)
+}
+# open server connection

inst/scraping_script/pigeon_scraping.R
@@ -1,0 +1,9 @@
+# Scrapping script -----
+
+start_chrome_remDr(kill = TRUE)
+
+remDr <- connect_remDr()
+
+css_query_tbl <- pigeon_query_builder(remDr)
+
+pigeon_scrapper(css_query_tbl[1,], remDr)

man/start_chrome_remDr.Rd
@@ -4,7 +4,7 @@
 \alias{start_chrome_remDr}
 \title{Start remote driver with docker using the system command line.}
 \usage{
-start_chrome_remDr(kill = TRUE)
+start_chrome_remDr(kill)
 }
 \arguments{
 \item{kill}{Bolean indicating weather a runing docker container with selenium

NAMESPACE
@@ -6,3 +6,4 @@ import(rlang)
 importFrom(rlang,.data)
 importFrom(rlang,.env)
 importFrom(stringr,fixed)
+importFrom(utils,capture.output)

R/query_scraping_funs.R
@@ -47,6 +47,7 @@ pigeon_query_builder <-
 pigeon_scrapper <-
   function(css_query_entry,
            remDr) {
+
     # Extract race html options
     race_html <- extract_race_html_options(css_query_tbl = css_query_entry,
                                            remDr = remDr)

man/remDr_go_to_link.Rd
@@ -1,0 +1,19 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/selenium_fun.R
+\name{remDr_go_to_link}
+\alias{remDr_go_to_link}
+\title{Connect remote driver to specified url}
+\usage{
+remDr_go_to_link(remDr, link)
+}
+\arguments{
+\item{remDr}{remote driver connection}
+
+\item{link}{link to the desired webpage}
+}
+\value{
+no return
+}
+\description{
+Connect remote driver to specified url
+}

tests/testthat/test-query_scraping_funs.R
@@ -29,3 +29,18 @@ test_that("query contains years on the years column", {
   expect_true(query_years %in% c(2010:2020) %>% unique)
 })
 
+# Scraping function -----
+#
+# start_chrome_remDr(kill = TRUE)
+#
+# remDr <- connect_remDr()
+# remDr_go_to_link()
+# page_source <- get_page_source(
+#   remDr = remDr,
+#   link = "https://pigeon-ndb.com/races/")
+#
+# years <- extract_years(parsed_html = page_source)
+#
+# css_query_tbl <- extract_orgs(years, remDr = remDr)
+#
+# pigeon_scrapper(css_query_tbl[1,], remDr)

commit 3af261d6033ec800bbe252c4118009bd92f56f17

fixed bug introduced by 5bc1ff88 on start_chrome_remDr

R/selenium_fun.R
@@ -7,7 +7,7 @@
 #' @import RSelenium
 #' @importFrom utils capture.output
 
-start_chrome_remDr <- function(kill) {
+start_chrome_remDr <- function(kill = FALSE) {
   ps_out <- system("docker ps", intern = TRUE)
 
   capt_ps_out <- capture.output(cat(ps_out))
@@ -30,13 +30,12 @@ start_chrome_remDr <- function(kill) {
 
     system(stringr::str_c("docker kill ", docker_id))
     Sys.sleep(2)
-    system("docker run -d -p 4445:4444 --shm-size 2g selenium/standalone-chrome")
-    Sys.sleep(2)
-  } else {
-    print("startign selenium/standalone-chrome docker container")
-    system("docker run -d -p 4445:4444 --shm-size 2g selenium/standalone-chrome")
-    Sys.sleep(2)
-  }
+    }
+
+  print("startign selenium/standalone-chrome docker container")
+  system("docker run -d -p 4445:4444 --shm-size 2g selenium/standalone-chrome")
+  Sys.sleep(2)
+
 }
 
 # connect to remote driver ---------------------------

inst/scraping_script/pigeon_scraping.R
@@ -1,6 +1,6 @@
 # Scrapping script -----
 
-start_chrome_remDr(kill = TRUE)
+start_chrome_remDr(kill = FALSE)
 
 remDr <- connect_remDr()
 

man/start_chrome_remDr.Rd
@@ -4,7 +4,7 @@
 \alias{start_chrome_remDr}
 \title{Start remote driver with docker using the system command line.}
 \usage{
-start_chrome_remDr(kill)
+start_chrome_remDr(kill = FALSE)
 }
 \arguments{
 \item{kill}{Bolean indicating weather a runing docker container with selenium

tests/testthat/test-css_query_build.R
@@ -2,7 +2,7 @@ context("Testing css queries generating functions")
 
 # Testing extract_years function ---------------
 
-start_chrome_remDr(kill = TRUE)
+start_chrome_remDr(kill = FALSE)
 remDr <- connect_remDr()
 page_source <- get_page_source(
   remDr = remDr,

tests/testthat/test-parse_races.R
@@ -1,6 +1,6 @@
 context("test race tables parsing")
 
-start_chrome_remDr(kill = TRUE)
+start_chrome_remDr(kill = FALSE)
 remDr <- connect_remDr()
 page_source <- get_page_source(
   remDr = remDr,
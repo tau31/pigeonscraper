8c68f520244a9d918331363846cf020d347d4727
Tiago Cabaço <tiago.joao.cabaco@gmail.com>
2020-03-30 09:28:17 +0200

    changed scraping script. Tests are failing on starting remDr

--- inst/scraping_script/pigeon_scraping.R
+++ inst/scraping_script/pigeon_scraping.R
@@ -6,5 +6,19 @@ remDr <- connect_remDr()
 
 css_query_tbl <- pigeon_query_builder(remDr)
 
-pigeon_scrapper(css_query_tbl[1,], remDr)
+start_end <- create_start_end(3, nrow(css_query_tbl))
+
+start <- start_end[,1]
+end <- start_end[,2]
+
+tail(start_end)
+purrr::map(
+  1,
+  function(i){
+    purrr::map(start[i] : end[i],
+        purrr::safely(function(g) {
+          pigeon_scrapper(css_query_tbl[g, ], remDr)
+        }))
+  }
+)
 

tau31/pigeonscraper

commit 6bae13fcc31fd4a874a809e959d6c453b512837e

Fixed isssue with missing release time and removed pre-processing of band information.

R/parse_races.R
@@ -228,6 +228,8 @@ assemble_tbl <- function(
 
       race_info_tbl <-
         tibble::tibble(
+          raw_info = race_html[[i]] %>%
+            rvest::html_text(),
           year = css_query_tbl$year,
           organization = css_query_tbl$organization,
           org_number = css_query_tbl$org_number,
@@ -329,38 +331,18 @@ pre_process_tbls <- function(tbls_list) {
             )
 
           temp_tbl <- temp_tbl %>%
-            dplyr::mutate(Pos = as.integer(.data$Pos),
-                          Section = dplyr::case_when(
-                            .data$Section == "" ~ NA_character_,
-                            .data$Section == "NA" ~ NA_character_,
-                            TRUE ~ .data$Section
-                          ),
-                          # Arrival = Arrival %>% lubridate::hms()
-                          Miles = as.numeric(.data$Miles),
-                          # to_win = as.numeric(to_win),
-                          YPM = as.numeric(.data$YPM),
-                          bird_birth_year = purrr::pmap_chr(
-                            list(.data$Band),
-                            function(b) {
-                              stringr::str_split(b, pattern = " ", simplify = TRUE)[3]
-                            }
-                          ),
-                          bird_national_org = purrr::pmap_chr(
-                            list(.data$Band),
-                            function(b) {
-                              stringr::str_split(b, pattern = " ", simplify = TRUE)[2]
-                            }
-                          ),
-                          bird_club_code = stringr::str_c(
-                            .data$bird_national_org,
-                            purrr::pmap_chr(
-                              list(.data$Band),
-                              function(b) {
-                                stringr::str_split(b, pattern = " ", simplify = TRUE)[4]
-                              }
-                            ),
-                            sep = "_"
-                          ))
+            dplyr::mutate(
+              Pos = as.integer(.data$Pos),
+              Section = dplyr::case_when(
+                .data$Section == "" ~ NA_character_,
+                .data$Section == "NA" ~ NA_character_,
+                TRUE ~ .data$Section
+              ),
+              # Arrival = Arrival %>% lubridate::hms()
+              Miles = as.numeric(.data$Miles),
+              # to_win = as.numeric(to_win),
+              YPM = as.numeric(.data$YPM)
+              )
           temp_tbl <- temp_tbl %>%
             dplyr::select(-.data$n, -.data$`NDB Points`)
 

commit ea2ebccb3728ee2176538b52467dd3f8ed2b6558

Fixed bug introduced by 6bae13fc in the assemble_table function.

DESCRIPTION
@@ -13,6 +13,8 @@ License: MIT + file LICENSE
 Encoding: UTF-8
 LazyData: true
 Suggests:
+    testthat (>= 2.1.0)
+Imports:
     dplyr, 
     furrr,
     future, 
@@ -25,7 +27,6 @@ Suggests:
     RSelenium,
     rvest,
     stringr,
-    testthat (>= 2.1.0),
     tibble,
     xml2
 RoxygenNote: 6.1.1

NAMESPACE
@@ -7,4 +7,5 @@ importFrom(future,multiprocess)
 importFrom(rlang,.data)
 importFrom(rlang,.env)
 importFrom(stringr,fixed)
+importFrom(stringr,regex)
 importFrom(utils,capture.output)

R/parse_races.R
@@ -151,16 +151,20 @@ race_table_parse <-
 #' \code{\link{race_table_parse}}.
 #' @param css_query_tbl individual css_query table used to access the
 #'  individual race
+#' @param race_html xml_nodeset of parsed html race options. Used to extract
+#' accurate information about race locations and start time.
 #'
 #' @return \code{list} with two tables: \code{race_results}
 #' and \code{race_info}
 #'
 #' @importFrom rlang .data
 #' @importFrom stringr fixed
+#' @importFrom stringr regex
 
 assemble_tbl <- function(
   races_xml,
-  css_query_tbl) {
+  css_query_tbl,
+  race_html) {
   . <- NULL
 
   seq_xml_docs <- 1:length(races_xml)
@@ -273,11 +277,11 @@ assemble_tbl <- function(
           #   "_race_",
           #   i
           # )
-          race_id = str_c(
+          race_id = stringr::str_c(
             "y",
-            str_extract(css_query_tbl$css_query_year, regex("\\d{1,}")),
+            stringr::str_extract(css_query_tbl$css_query_year, regex("\\d{1,}")),
             "o",
-            str_extract(css_query_tbl$css_query_org, regex("\\d{1,}")),
+            stringr::str_extract(css_query_tbl$css_query_org, regex("\\d{1,}")),
             "r",
             i
           )

R/query_scraping_funs.R
@@ -59,7 +59,9 @@ scraper <-
     xml_doc <- race_table_parse(race_xml_nodeset = race_html, remDr= remDr)
 
     # Extract tables into tibbles and assemble race_results and race_info tbls
-    raw_tbls <- assemble_tbl(races_xml = xml_doc,css_query_tbl = css_query_entry)
+    raw_tbls <- assemble_tbl(races_xml = xml_doc,
+                             css_query_tbl = css_query_entry,
+                             race_html = race_html)
 
     # Pre-process tables
     tbls_list <- pre_process_tbls(raw_tbls)
@@ -83,8 +85,8 @@ pigeon_scraper <-
 
     cat("Checking if path data/raw_data exists
         if not, one will be created \n")
-    if(dir.exists(here::here("data", "raw_data")) == FALSE) {
-      dir.create(here::here("data", "raw_data"), recursive = TRUE)
+    if(dir.exists(here::here("inst", "raw_data")) == FALSE) {
+      dir.create(here::here("inst", "raw_data"), recursive = TRUE)
     }
 
     start_chrome_remDr(kill = FALSE)
@@ -96,15 +98,13 @@ pigeon_scraper <-
     cat("building css query \n")
     css_query_tbl <- pigeon_query_builder(remDr = remDr)
 
-    cat("saving css_query_tbl as .rds")
+    cat("saving css_query_tbl as .rds \n")
     saveRDS(css_query_tbl, here::here("inst", "css_query", "css_query_tbl.rds"))
 
 
     cat("scraping function \n")
     purrr::walk(
       1:nrow(css_query_tbl),
-      # 5296:nrow(css_query_tbl),
-      # 1855,
       function(i){
         print(i)
         temp_race_data  <- purrr::map(

man/assemble_tbl.Rd
@@ -4,7 +4,7 @@
 \alias{assemble_tbl}
 \title{Assembre race tables from xml documents}
 \usage{
-assemble_tbl(races_xml, css_query_tbl)
+assemble_tbl(races_xml, css_query_tbl, race_html)
 }
 \arguments{
 \item{races_xml}{xml documents with race tables geneerated from
@@ -12,6 +12,9 @@ assemble_tbl(races_xml, css_query_tbl)
 
 \item{css_query_tbl}{individual css_query table used to access the
 individual race}
+
+\item{race_html}{xml_nodeset of parsed html race options. Used to extract
+accurate information about race locations and start time.}
 }
 \value{
 \code{list} with two tables: \code{race_results}
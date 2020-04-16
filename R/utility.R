998b6017da7e1730c3e02efeecde5b68c935a3aa
Tiago Cabaço <tiago.joao.cabaco@gmail.com>
2020-04-16 06:51:09 +0200

    Improved package dependency structure.

--- R/utility.R
+++ R/utility.R
@@ -20,20 +20,19 @@ create_start_end <- function(seq_length, rows) {
 }
 
 # future plan multiprocess with user defined number of processes.
-
-#' Function to plan parallelization plan.
-#'
-#' In order to parallize multiple scraping tasks over many different cores, we
-#' need to set our parallization plan. By default, \code{pigeonscraper} uses
-#' \code{plan(multiprocess)} and the user can set the number of cores to be used
-#' by setting the parameter workers.
-#'
-#' @param workers number of cores to be used in parallel.
-#'
-#' @importFrom future multiprocess
-
-plan_future <- function(workers){
-  options(mc.cores = workers)
-  cat(paste0("Using ", future::availableCores("mc.cores"), " cores \n"))
-  future::plan(multiprocess)
-}
+# #' Function to plan parallelization plan.
+# #'
+# #' In order to parallize multiple scraping tasks over many different cores, we
+# #' need to set our parallization plan. By default, \code{pigeonscraper} uses
+# #' \code{plan(multiprocess)} and the user can set the number of cores to be used
+# #' by setting the parameter workers.
+# #'
+# #' @param workers number of cores to be used in parallel.
+# #'
+# #' @importFrom future multiprocess
+#
+# plan_future <- function(workers){
+#   options(mc.cores = workers)
+#   cat(paste0("Using ", future::availableCores("mc.cores"), " cores \n"))
+#   future::plan(multiprocess)
+# }
